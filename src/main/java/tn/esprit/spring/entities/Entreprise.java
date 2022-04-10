d8957e7b6c560974d80c5938355fbf03f180d3fd
sendanemissi <senda.nemissi@esprit.tn>
2022-04-10 15:13:32 +0100

    add entities and packages

---
+++ src/main/java/tn/esprit/spring/entities/Entreprise.java
@@ -0,0 +1,89 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Entreprise implements Serializable{
+	
+	private static final long serialVersionUID = 3152690779535828408L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String raisonSocial;
+	
+	@OneToMany(mappedBy="entreprise", 
+			cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, 
+			fetch=FetchType.EAGER)
+	private List<Departement> departements = new ArrayList<>();
+
+	@JsonIgnore
+	@ManyToMany
+	private List<Partner> partners;
+	
+	public List<Partner> getPartners() {
+		return partners;
+	}
+
+	public void setPartners(List<Partner> partners) {
+		this.partners = partners;
+	}
+
+	public Entreprise() {
+		super();
+	}
+
+	public Entreprise(String name, String raisonSocial) {
+		this.name = name;
+		this.raisonSocial = raisonSocial;
+	}
+ 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRaisonSocial() {
+		return raisonSocial;
+	}
+
+	public void setRaisonSocial(String raisonSocial) {
+		this.raisonSocial = raisonSocial;
+	}
+
+	public List<Departement> getDepartements() {
+		return departements;
+	}
+
+	public void setDepartements(List<Departement> departements) {
+		this.departements = departements;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
